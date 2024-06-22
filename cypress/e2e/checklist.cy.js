2035f347dd494ac181a77f6f1bc9c64e72fefd64
Walmyr <wlsf82@gmail.com>
2024-06-22 20:37:46 +0200

    Fix test description

--- cypress/e2e/checklist.cy.js
+++ cypress/e2e/checklist.cy.js
@@ -8,7 +8,7 @@ describe('Cypress Skills Checklist', () => {
   })
 
   context('Perncentages', () => {
-    it('checking one checkbox changes the percentage from zero to something', () => {
+    it('checking one checkbox changes the percentage from zero to something else', () => {
       cy.contains('footer', '0%').should('be.visible')
 
       cy.get('main input[type="checkbox"]').first().check()
