5830e947098a33ed977384dd426c58a5236aa0a6
Walmyr <wlsf82@gmail.com>
2024-06-22 18:52:53 +0200

    Small app adjustments
    - Text adjustments
    - Comments removal
    - favicon addition

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]')
   const checkedCountElement = document.getElementById('checkedCount')
   const totalCountElement = document.getElementById('totalCount')
@@ -13,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const percentage = (checkedCount / checkboxes.length * 100).toFixed(0)
     percentageElement.textContent = percentage
 
-    // Update skill level based on percentage
     if (percentage < 50) {
       skillLevelElement.textContent = 'Iniciante (Jr.)'
     } else if (percentage >= 50 && percentage < 75) {
@@ -32,22 +31,22 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 // Check for saved user theme preference, if any, and apply it
-document.addEventListener('DOMContentLoaded', function() {
-  const savedTheme = localStorage.getItem('theme') || 'light'; // Default to light theme
-  document.documentElement.setAttribute('data-theme', savedTheme);
-  updateToggleButton(savedTheme); // Set the button text based on the saved theme
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme') || 'light' // Default to light theme
+  document.documentElement.setAttribute('data-theme', savedTheme)
+  updateToggleButton(savedTheme) // Set the button text based on the saved theme
+})
 
-document.getElementById('themeToggle').addEventListener('click', function() {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  document.documentElement.setAttribute('data-theme', newTheme);
-  localStorage.setItem('theme', newTheme);
-  updateToggleButton(newTheme); // Update the button text based on the new theme
-});
+document.getElementById('themeToggle').addEventListener('click', () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme')
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+  document.documentElement.setAttribute('data-theme', newTheme)
+  localStorage.setItem('theme', newTheme)
+  updateToggleButton(newTheme) // Update the button text based on the new theme
+})
 
 function updateToggleButton(theme) {
-  const themeToggle = document.getElementById('themeToggle');
-  themeToggle.textContent = theme === 'dark' ? '☀' : '☽'; // Set text as sun for light mode, moon for dark mode
-  themeToggle.setAttribute('aria-label', theme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode'); // Accessibility improvement
+  const themeToggle = document.getElementById('themeToggle')
+  themeToggle.textContent = theme === 'dark' ? '☀' : '☽' // Set text as sun for light mode, moon for dark mode
+  themeToggle.setAttribute('aria-label', theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode') // Accessibility improvement
 }
